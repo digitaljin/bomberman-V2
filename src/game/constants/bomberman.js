0ff3f3a930a9d78a809015e985d49c92d6ee25f3
digitaljin <digitaljindojo@gmail.com>
2023-10-29 20:04:25 -0700

    1st commit

---
+++ src/game/constants/bomberman.js
@@ -0,0 +1,53 @@
+import { Direction } from "./entities.js";
+
+export const WALK_SPEED = 40;
+
+export const BombermanStateType = {
+  IDLE: 'idle',
+  MOVING: 'moving',
+};
+
+export const frames = new Map([
+  ['idle-down', [[4, 5, 17, 22], [8, 15]]],
+  ['move-down-1', [[30, 5, 17, 22], [7, 15]]],
+  ['move-down-2', [[61, 5, 17, 22], [9, 15]]],
+  ['idle-side', [[79, 5, 18, 22], [7, 15]]],
+  ['move-side-1', [[104, 5, 17, 21], [8, 15]]],
+  ['move-side-2', [[129, 5, 18, 22], [8, 15]]],
+  ['idle-up', [[154, 4, 17, 22], [8, 15]]],
+  ['move-up-1', [[180, 4, 17, 22], [7, 15]]],
+  ['move-up-2', [[211, 4, 17, 22], [9, 15]]],
+  ['idle-down-left', [[5, 55, 17, 20], [6, 15]]],
+  ['idle-up-left', [[30, 55, 17, 20], [6, 15]]],
+
+  ['death-1', [[10, 30, 21, 20], [10, 15]]],
+  ['death-2', [[44, 30, 19, 19], [9, 15]]],
+  ['death-3', [[75, 30, 21, 20], [11, 15]]],
+  ['death-4', [[108, 30, 22, 20], [11, 15]]],
+  ['death-5', [[142, 31, 20, 19], [10, 15]]],
+  ['death-6', [[175, 32, 20, 19], [10, 15]]],
+  ['death-7', [[207, 33, 21, 19], [11, 15]]],
+  ['death-8', [[240, 32, 22, 21], [11, 15]]],
+  ['death-9', [[273, 32, 22, 21], [11, 15]]],
+]);
+
+export const animations = {
+  moveAnimations: {
+    [Direction.LEFT]: [
+      ['idle-side', 8], ['move-side-1', 8], ['idle-side', 8], ['move-side-2', 8]
+    ],
+    [Direction.RIGHT]: [
+      ['idle-side', 8], ['move-side-1', 8], ['idle-side', 8], ['move-side-2', 8]
+    ],
+    [Direction.UP]: [
+      ['idle-up', 8], ['move-up-1', 8], ['idle-up', 8], ['move-up-2', 8]
+    ],
+    [Direction.DOWN]: [
+      ['idle-down', 8], ['move-down-1', 8], ['idle-down', 8], ['move-down-2', 8]
+    ],
+  },
+  deathAnimations: [
+    ['death-1', 6], ['death-2', 6], ['death-1', 6], ['death-2', 6], ['death-1', 6], ['death-2', 6], ['death-3', 6],
+    ['death-4', 6], ['death-5', 6], ['death-6', 6], ['death-7', 6], ['death-6', 6], ['death-9', 6], ['death-9', -1],
+  ],
+};
